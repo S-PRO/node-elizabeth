9f052e3eb498b8457a8179049d9a9346a13d7897
kentserg94 <kentserg1994@mail.ru>
2017-05-21 21:39:52 +0300

    tested address,code,file,text

---
+++ src/tests/code.test.js
@@ -0,0 +1,33 @@
+import { Code } from "../providers.js";
+import {LOCALE_CODES} from "../locales/int/code.js";
+
+const code = new Code();
+
+describe('Test code provider', () => {
+
+  it('should return item from 4 digits', () => {
+    expect(code.pin()).toMatch(/^[0-9]{4}$/);
+  });
+
+  it('should return mask from 4 chars', () => {
+    expect(code.customCode({mask: "@###", char: '@', digit: '#'})).toHaveLength(4);
+  });
+
+  it('should return random item from array', () => {
+    expect(LOCALE_CODES).toContain(code.localeCode());
+  });
+
+  it('should return item from 8  and 13 digits', () => {
+    console.log(code.ean('ean-8'));
+    expect(code.ean({format: 'ean-8'})).toMatch(/^[0-9]{8}$/);
+    expect(code.ean({format: 'ean-13'})).toMatch(/^[0-9]{13}$/);
+  })
+
+  it('should return mask fromat 4 digits - 4 digits', () => {
+    expect(code.issn()).toMatch(/^[0-9]{4}-[0-9]{4}$/);
+  });
+
+  it('should return string <= 15 digits', () => {
+    expect(code.imei()).toMatch(/^\d{14,15}$/);
+  });
+});
